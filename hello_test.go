e72a9d7dda42bdd56b3b0a77ee050127a8c49e75
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-15 17:03:13 -0500

    refactor into one test with subtests

--- hello_test.go
+++ hello_test.go
@@ -2,20 +2,22 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "hello and welcome!" 
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+func TestStringMatching(t *testing.T) {
+	assertStringsMatch := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
-}
 
-func TestIMadeThis(t *testing.T) {
-	got := IMadeThis()
-	want := "i made this http server!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("testing hello function", func(t *testing.T) {
+		got := Hello()
+		want := "hello and welcome!"
+		assertStringsMatch(t, got, want)
+	})
+	t.Run("testing i made this function", func(t *testing.T) {
+		got := IMadeThis()
+		want := "i made this http server!"
+		assertStringsMatch(t, got, want)
+	})
 }
