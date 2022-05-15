cf5f3e18d901b2113f602ff38b2370b0b11ec330
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-15 15:01:42 -0500

    extract string methods

--- hello.go
+++ hello.go
@@ -6,15 +6,23 @@ import (
 	"net/http"
 )
 
+func IMadeThis() string {
+	return "i made this http server!"
+}
+
+func Hello() string {
+	return "hello and welcome!"
+}
+
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "i made this http server!")
+        fmt.Fprintf(w, IMadeThis())
     })
 
     http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "hello and welcome!")
+        fmt.Fprintf(w, Hello())
     })
  
     http.ListenAndServe(":5050", nil)
