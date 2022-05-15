80778c014e42bcc410c067f92147d74ab4de06c9
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-15 14:34:38 -0500

    add new hello route

--- hello.go
+++ hello.go
@@ -12,6 +12,10 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "i made this http server!")
     })
+
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "hello and welcome!")
+    })
  
     http.ListenAndServe(":5050", nil)
 }
