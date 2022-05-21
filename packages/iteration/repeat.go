47a151cfa445f2fe185deeb84ac1cc8c6d6e12c6
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-21 12:09:04 -0500

    add docs

--- packages/iteration/repeat.go
+++ packages/iteration/repeat.go
@@ -1,5 +1,7 @@
 package iteration
 
+// Repeat takes a character as input and returns 
+// a string of that character repeated 5 times
 func Repeat(character string) string {
 	var repeated string
 	for i := 0; i < 5; i++ {
