12998f561b96ee7d82d584966cbb90eb5d504847
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-22 23:05:08 -0500

    add circle

--- packages/shapes/shapes_test.go
+++ packages/shapes/shapes_test.go
@@ -2,6 +2,7 @@ package shapes
 
 import "testing"
 
+
 func TestPerimieter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
@@ -13,11 +14,24 @@ func TestPerimieter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func (t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func (t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %2.f want %2.f", got, want)
+		}
+	})
 }
