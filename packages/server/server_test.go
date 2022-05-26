ae712872c4e2223a1b1a34935a5cb2f485d7df4c
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-25 21:35:39 -0500

    create initial server

---
+++ packages/server/server_test.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"http"
+	"httptest"
+    "testing"
+)
+
+
+func TestGETPlayers(t *testing.T) {
+	t.run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+
+	})
+}
