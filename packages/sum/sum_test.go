15db58e1b5d0f5433dd82720606678317409ed87
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-29 18:48:47 -0500

    add test case and refactor

--- packages/sum/sum_test.go
+++ packages/sum/sum_test.go
@@ -6,15 +6,21 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
+	checkSum := func(t testing.TB, collection []int, want int) {
+		got := Sum(collection)
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, collection)
+		}
+	}
+
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		checkSum(t, numbers, 15)
+	})
+
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
+		checkSum(t, numbers, 6)
 	})
 	
 }
