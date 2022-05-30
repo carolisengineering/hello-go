334c8e7442b9398099d9d4370d23a22652b6c15e
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-05-30 13:13:22 -0500

    reorder structs and functions

--- packages/shapes/shapes.go
+++ packages/shapes/shapes.go
@@ -6,6 +6,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -14,10 +19,7 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-type Rectangle struct {
-	Width float64
-	Height float64
-}
+
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
